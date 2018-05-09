2ca8a2da2d58db28a91ed3f39dae6b9eebe6e7a0
Chase Schachenman <chaseshak@gmail.com>
2018-05-09 16:16:02 -0500

    Partially setup fortnite node api

--- bot.js
+++ bot.js
@@ -1,6 +1,25 @@
-const Discord = require("discord.js");
 var auth = require("./auth.json")
+
+const Discord = require("discord.js");
 const bot = new Discord.Client();
+const Fortnite = require("fortnite-api");
+
+let fortniteAPI = new Fortnite(
+    [
+        auth.fortnite.email,
+        auth.fortnite.password,
+        auth.fortnite.launcher_token,
+        auth.fortnite.client_token
+    ],
+    {
+        debug: true
+    }
+);
+
+fortniteAPI.login().then(() => {
+    //TODO: https://www.npmjs.com/package/fortnite-api
+    // Must first add launcher and client token. See link for details
+});
 
 bot.on("ready", () => {
   console.log("I am ready!");
@@ -11,6 +30,7 @@ bot.on("message", (message) => {
   if (message.content.startsWith("!FNP help")) {
     message.channel.send("A bot to track your fortnite progress over time. ```COMMANDS GO HERE```");
   }
+  if (message.content.startsWith("!FNP "))
 });
 
 bot.login(auth.token);
